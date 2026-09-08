434d3a735051d4cd4f4af09ab14e245996859e6f
GerryAdom <gerraldadomako639@gmail.com>
2026-09-08 18:05:02 +0000

    Add files via upload

---
+++ electricalMeasurementConsole.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        voltage = float(input('Enter voltage: '))
+        current = float(input('Enter current: '))
+        
+        if voltage < 0 or current < 0:
+            print("Invalid inputs")
+            continue
+        
+        power = voltage * current
+        
+        print(f"Current: {current} A")
+        print(f"Voltage: {voltage} V")
+        print(f"Power: {power:.2f} W")
+        
+        choice = input("Do you want to enter another measurement? (yes/no): ")
+        
+        if choice == "no":
+            break
+    
+    except ValueError:
+        print('Enter numbers')
+    
